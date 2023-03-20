5d921da4023f753db21f4801fc0bb9e5c1fd4183
4ndroid3 <andijaimo@gmail.com>
2023-03-20 09:30:00 -0300

    Modified errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,16 +1,16 @@
 """
-Test cyustom Django management commands.
+Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.managment.commands.wait_for_db.Command.check')
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
@@ -20,4 +20,4 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
